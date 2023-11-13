afb10f93986719f6339d7dea750fdb35da5775a3
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-11-13 11:49:57 -0800

    0-prod_output.c

--- 0-prod_output.c
+++ 0-prod_output.c
@@ -1,45 +1,81 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
- * _printf - produces output according to a format
- * @format: format.
- * Return: Printed chars
+ * _printf - Custom printf function
+ * @format: Format string.
+ * Return: Number of characters printed (excluding the null byte).
  */
-
-int _printf(const char *format, ...)
+int _printf(const char(*format), ...)
 {
-	int count = 0;
+	if (format == NULL)
+	return (-1);
 
+	int printed_chars = 0;
 	va_list args;
 
 	va_start(args, format);
 
-	while (*format != '\0')
+	while (*format)
 	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-				count += putchar(va_arg(args, int));
-				break;
-				case 's':
-				count += printf("%s", va_arg(args, char *));
-				break;
-				case '%':
-				count += putchar('%');
-				break;
-				default:
-				return (-1);
-			}
-		}
-		else
-		{
-			count += putchar(*format);
-		}
-		format++;
+		if (*format != '%')
+	{
+		printed_chars += putchar(*format);
 	}
+	else
+	{
+		format++;
+		switch (*format)
+	{
+		case 'c':
+			printed_chars += putchar(va_arg(args, int));
+			break;
+		case 's':
+			printed_chars += puts(va_arg(args, char *));
+			break;
+		case '%':
+			printed_chars += putchar('%');
+			break;
+		default:
+			printed_chars += putchar('%') + putchar(*format);
+			break;
+	}
+	}
+
+	format++;
+	}
+
 	va_end(args);
-	return (count);
+	return (printed_chars);
+}
+
+/**
+ * _puts - Custom function to print a string
+ * @str: String to be printed.
+ * Return: Number of characters printed.
+ */
+int _puts(char *str)
+{
+	if (str == NULL)
+	str = "(null)";
+
+	int i = 0;
+
+	while
+		(str[i]);
+
+	putchar(str[i++]);
+
+	return (i);
+}
+
+/**
+ * putchar - Custom putchar function
+ * @c: Character to be printed.
+ * Return: 1 (success).
+ */
+int putchar(int i)
+{
+	return fputs(int, stdout);
 }
--- main.h
+++ main.h
@@ -1,18 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
-int _printf(const char *format, ...);
-
-#endif
-=======
 /*#include <unistd.h>
 #include <ctype.h>*/
 #include <stdio.h>
 #include <stdarg.h>
+#include <stddef.h>
+
+#define BUFSIZE 1024
 
 int _printf(const char *format, ...);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int handle_print(const char *format, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 #endif
-
->>>>>>> 338e4157c8209686dc2424f9773ead367d3d7679
